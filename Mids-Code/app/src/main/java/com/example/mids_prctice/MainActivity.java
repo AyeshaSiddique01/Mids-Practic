2e6ad56d7fd554f67d1ffed1bbbe3acfcde0879d
AyeshaSiddique01 <89693571+AyeshaSiddique01@users.noreply.github.com>
2022-12-12 22:51:14 +0500

    Lecture 4 completed

--- Mids-Code/app/src/main/java/com/example/mids_prctice/MainActivity.java
+++ Mids-Code/app/src/main/java/com/example/mids_prctice/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.mids_prctice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.textView);
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn.setText("Clicked");
+            }
+        });
     }
+//
+//    public void ChangeText(View view) {
+//        tv.setText("Button Clicked");
+//    }
 }
